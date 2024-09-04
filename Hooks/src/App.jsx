9e9abb141be511cd8698e6aaf97a92d545acb402
Meer-Athif-Ali-I <meerathifali@gmail.com>
2024-09-04 12:34:45 +0530

    UseState with previous state

--- Hooks/src/App.jsx
+++ Hooks/src/App.jsx
@@ -3,24 +3,20 @@ import './App.css'
 import {useState} from 'react';
 
 function App() {
-const [car, setCar] =useState({
-  brand: "Ferrari",
-  model: "Roma",
-  year: "2023",
-  color: "Red"
-});
-
- const changeColor = ()=>{
-  setCar((car)=>{
-    return {...car, color:"blue"}
-  })
+ const [count, setCount] = useState(0);
+ const increaseCount=()=>{
+  setCount(count=>count+1)
+  setCount(count=>count+1);
+  setCount(count=>count+1);
+  setCount(count=>count+1);
  }
+
+ 
 
   return (
     <>
-    <h1>My {car.brand}</h1>
-    <h2>It is a {car.color} {car.model} from {car.year}</h2>
-    <button onClick={changeColor}>Blue</button>
+    <h1>Count: {count}</h1>
+    <button onClick={increaseCount}>Increase by 4</button>
     </>
   )
 }
