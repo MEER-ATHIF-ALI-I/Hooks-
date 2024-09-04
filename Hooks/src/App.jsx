b69fea792eb542f48356bb4d773fba021d3dc52c
Meer-Athif-Ali-I <meerathifali@gmail.com>
2024-09-04 16:36:30 +0530

    accessing DOM elements using useRef

--- Hooks/src/App.jsx
+++ Hooks/src/App.jsx
@@ -3,19 +3,17 @@ import './App.css'
 
 
 function App() {
-  const [value, setValue] =useState(0)
-  const count= useRef(0);
-  
-  useEffect(()=>{
-    count.current=count.current +1;
-  })
-  
+ 
+ const inputElem= useRef()
+
+const btnClicked=()=>{
+  console.log(inputElem.current);
+  inputElem.current.style.background= "blue";
+}
   return (
     <>
-   <button onClick={()=>{setValue(prev=>prev -1)}}>-1</button>
-   <h1>{value}</h1>
-   <button onClick={()=>{setValue(prev=>prev +1)}}>+1</button>
-   <h1>Render Count: {count.current}</h1>
+  <input type="text" ref={inputElem}/>
+  <button onClick={btnClicked}>Click Here</button>
     </>
   )
 }
