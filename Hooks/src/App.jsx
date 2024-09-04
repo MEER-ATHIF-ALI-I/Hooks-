61d5821ef3c2bf65ff51c4acd2b89d10cfb0dcab
Meer-Athif-Ali-I <meerathifali@gmail.com>
2024-09-04 16:27:05 +0530

    now useRef does not trigger re-render

--- Hooks/src/App.jsx
+++ Hooks/src/App.jsx
@@ -1,21 +1,21 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import './App.css'
 
 
 function App() {
   const [value, setValue] =useState(0)
-  const [count, setCount] =useState(0)
-
-
+  const count= useRef(0);
+  
   useEffect(()=>{
-    setCount(prev=>prev+1)
+    count.current=count.current +1;
   })
+  
   return (
     <>
    <button onClick={()=>{setValue(prev=>prev -1)}}>-1</button>
    <h1>{value}</h1>
    <button onClick={()=>{setValue(prev=>prev +1)}}>+1</button>
-   <h1>Render Count:{count}</h1>
+   <h1>Render Count: {count.current}</h1>
     </>
   )
 }
