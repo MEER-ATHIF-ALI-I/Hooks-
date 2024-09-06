60b4bb625d7d03c7e79c8ecfba7de914232288fa
Meer-Athif-Ali-I <meerathifali@gmail.com>
2024-09-06 17:52:54 +0530

    useReducer Hook

--- Hooks/src/App.jsx
+++ Hooks/src/App.jsx
@@ -3,14 +3,46 @@ import './App.css'
 import Header from './components/Footer';
 import Profile from './components/Profile';
 import Footer from './components/Footer';
+import { useReducer } from 'react';
 
 
 function App() {
+  const initialState={count:0}
+
+  const reducer =(state, action)=>{
+    switch(action.type){
+      case 'increase' : {
+        return {count:state.count + 1}
+      }
+      case 'decrease' :{
+        return {count:state.count - 1}
+      }
+      case 'input' : {
+        return {count: action.payload}
+      }
+      default:{
+        return state
+      }
+    }
+    
+  }
+  
+  const[state,dispatch] = useReducer(reducer, initialState)
    
   return (
     <>
-    <Profile/>
-    <Footer/>
+    <h1>{state.count}</h1>
+    <button onClick={()=>dispatch({type:'increase'})}>Increase</button>
+
+    <button onClick={()=>dispatch({type:'decrease'})}>Decrease</button>
+    <br/>
+    <input value={state.count}
+    onChange={(e)=>dispatch({type:'input', payload: Number(e.target.value)})}
+     type="number"/>
+
+
+    {/* <Profile/>
+    <Footer/> */}
     </>
   )
 }
